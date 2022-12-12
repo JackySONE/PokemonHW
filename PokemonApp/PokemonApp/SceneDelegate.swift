ec3a7b827781cbf01880be2c8f42d6c06a014bd5
賴彥宇 <jackysnsd@gmail.com>
2022-12-12 11:15:21 +0800

    Add Emptt PokemonApp

---
+++ PokemonApp/PokemonApp/SceneDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  SceneDelegate.swift
+//  PokemonApp
+//
+//  Created by 賴彥宇 on 2022/12/12.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let _ = (scene as? UIWindowScene) else { return }
+    }
+
+}
+
---
+++ PokemonApp/PokemonApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PokemonApp
+//
+//  Created by 賴彥宇 on 2022/12/12.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
