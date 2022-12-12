1f6cdf3d4c9a5d064993995d2093b332cb6609d3
賴彥宇 <jackysnsd@gmail.com>
2022-12-12 11:30:18 +0800

    Show Pokemon List

--- PokemonApp/PokemonApp/SceneDelegate.swift
+++ PokemonApp/PokemonApp/SceneDelegate.swift
@@ -6,14 +6,25 @@
 //
 
 import UIKit
+import Pokemon
+import PokemonIOS
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
 
-
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let _ = (scene as? UIWindowScene) else { return }
+        
+        let url = URL(string: "https://pokeapi.co/api/v2/pokemon")!
+        
+        let session = URLSession(configuration: .ephemeral)
+        let client = URLSessionHTTPClient(session: session)
+        let pokemonLoader = RemotePokemonLoader(url: url, client: client)
+        
+        let pokemonListViewController = PokemonListUIComposer.pokemonListComposedWith(pokemonLoader: pokemonLoader)
+        
+        window?.rootViewController = pokemonListViewController
     }
 
 }
